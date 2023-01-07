a74f99c7212d56b7cea37575c2e4415c6605c677
Seif Sekalala <dibleauzah@gmail.com>
2023-01-06 23:28:48 -0500

    Last_Gen-Update-Jan-6-2023

--- Server/index.js
+++ Server/index.js
@@ -3,6 +3,11 @@ const app = express();
 const mongoose = require('mongoose')
 const UserModel = require('./models/Users')
 
+const cors = require('cors');
+app.use(cors)
+
+app.use(express.json());
+
 mongoose.connect(
     "mongodb+srv://userSS1985:opensesame1@cluster0.x9gjx8y.mongodb.net/merntutorial?retryWrites=true&w=majority"
     );
@@ -17,6 +22,13 @@ mongoose.connect(
         });
     });
 
+app.post("/createUser", async (req, res) => {
+    const user = req.body;
+    const newUser = new UserModel(user);
+    await newUser.save();
+
+    res.json(user)
+});
 
 app.listen(3001, () => {
   console.log("SERVER RUNS WELL!");
